61756619526256d07796df315dc3bc634ffc9fd2
Anush Shukla <anushshukla@DESKTOP-3DE13QI.localdomain>
2017-09-11 00:04:58 +0530

    initial commit

---
+++ Request.php
@@ -0,0 +1,163 @@
+<?php 
+
+class Request
+{
+
+  private $_method;
+  private $_host;
+  private $_scheme;
+  private $_port;
+  private $_domain;
+  private $_uri;
+  private $_url;
+  private $_get;
+  private $_post;
+  private $_put;
+  private $_data = array();
+  private $_headers;
+  private $_isAjax;
+  private $_ip;
+  private $_content;
+  private CONST METHOD_GET = 'GET';
+  private CONST METHOD_HEAD = 'HEAD';
+  private CONST METHOD_POST = 'POST';
+  private CONST METHOD_PUT = 'PUT';
+  private CONST METHOD_DELETE = 'DELETE';
+  private CONST METHOD_OPTIONS = 'OPTIONS';
+  private CONST METHOD_TRACE = 'TRACE';
+  private CONST METHODS = [
+    Request::METHOD_GET
+    ,Request::METHOD_HEAD
+    ,Request::METHOD_POST
+    ,Request::METHOD_PUT
+    ,Request::METHOD_DELETE
+    ,Request::METHOD_OPTIONS
+    ,Request::METHOD_TRACE
+  ];
+
+  private function __construct()
+  {
+    $this->_method  = strtolower(@$_SERVER['REQUEST_METHOD']);
+    $this->_scheme  = $this->isSecure() ? 'https' : 'http';
+    $this->_host    = @$_SERVER['HTTP_HOST'];
+    $this->_port    = @$_SERVER['SERVER_PORT'];
+    $this->_domain  = @$_SERVER['SERVER_NAME'];
+    $this->_uri     = substr(strtok(@$_SERVER["REQUEST_URI"],'?'), 1);
+    $this->_url     = $this->_scheme . ':' . '//' . $this->_host . '/' . $this->uri;
+    $this->_get     = $_GET;
+    $this->_post    = $_POST;
+    $this->_files   = $_FILES;
+    $this->_body     = file_get_contents("php://input") ?? [];
+    $this->_data    = $_REQUEST + $this->_files;
+    if(!empty($this->body)) {
+      $this->_data += $this->body;
+    }
+    $this->_headers = getallheaders();
+    $this->_isAjax  = @$_SERVER['HTTP_X_REQUESTED_WITH'] && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+    $this->_ip      = @$_SERVER['HTTP_X_FORWARDED_FOR'] ?? @$_SERVER['REMOTE_ADDR'];
+    $this->content = $this->getHeader('Content-Type');
+    $contentArr = explode("/", $content);
+    $this->contentPrimary = $contentArr[0];
+    $this->contentSecondary = $contentArr[1] ?? '';
+  }
+
+  public static function init()
+  {
+    return new self(func_get_args());
+  }
+
+  private function detectRequestBody() {
+    $rawInput = fopen('php://input', 'r');
+    $tempStream = fopen('php://temp', 'r+');
+    stream_copy_to_stream($rawInput, $tempStream);
+    rewind($tempStream);
+    return $tempStream;
+  }
+
+  public function isSecure()
+  {
+    return
+        ( !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' && $_SERVER['HTTPS'] == 'on')
+        || ( strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https')
+        || ( !empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+        || ( !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on')
+        || (443 == $this->_port)
+        || (isset($_SERVER['HTTP_X_FORWARDED_PORT']) && $_SERVER['HTTP_X_FORWARDED_PORT'] == 443)
+        || (isset($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'https');
+  }
+
+  public function isMethod($method)
+  {
+    return $this->_method === strtolower(trim($method));
+  }
+
+  public function isContent($contentType, $contentTypeSecondary = NULL)
+  {
+    if(!$contentTypeSecondary) :
+      return $this->content === $contentType;
+    endif;
+    return $this->contentPrimary === $contentType && $this->contentSecondary === $contentTypeSecondary;
+  }
+
+  public static function __callStatic($method = '', array $arguments = array())
+  {
+    $obj = new self();
+    if(!method_exists($obj, $method) || $this->__get($method)) {
+      throw new Exception("$method does not exists", 1);
+    }
+    return call_user_method_array($method, $obj, $arguments);
+  }
+
+  public function __call($method, $arguments)
+  {
+    $method = strtoupper(str_replace('is', '', $method));
+    if(!in_array($method, Request::METHODS)) {
+      throw new Exception(get_class($this) ." does not have a method, $method.");
+    }
+    return $this->isMethod($method);
+  }
+
+  private function getHeader($headerName = "")
+  {
+    return isset($this->_headers[(string)$headerName])
+    ? strtolower(trim($this->_headers[(string)$headerName]))
+    : '';
+  }
+
+  public function __get($property) {
+    $pvtProp = '_'.$property;
+    if (property_exists($this,$pvtProp)) {
+      return $this->$pvtProp;
+    }
+    $getterFuncName = 'get'.ucfirst($property);
+    if (method_exists($this, $getterFuncName)) {
+      return $this->$getterFuncName;
+    }
+    throw new Exception(get_class($this) ." does not have a attribute, $property.");
+  }
+
+  public function getMethod() {
+    return $this->_method;
+  }
+
+  public function isSubDomain()
+  {
+    $hostArr = explode(".",$host);
+    $count = count($hostArr);
+    /*$hostArr = array_slice($a,1,-1);*/
+    if($count > 2) :
+      array_shift($hostArr);
+      if($count > 3) :
+          if($hostArr[0] === "demo") : array_shift($hostArr); endif;
+          /*if($hostArr[$count-1] === "wwww") : array_pop($hostArr); endif;*/
+      endif;
+    endif;
+    $domain = implode(".",$hostArr);
+    $arr["email"] = "info@".$domain;
+  }
+
+  function __destruct() {
+    // unset($this);
+  }
+
+}
